729bbeb1a01b2768c1e5797217da823dc5892ba7
cano <yra06052005@gmail.com>
2023-12-21 18:25:27 +0200

    .\home_work_12\

--- home_work_12/home_work12.py
+++ home_work_12/home_work12.py
@@ -16,19 +16,20 @@ for film in films_titles["results"]:
     film_directory = os.path.join(main_directory, film_title.replace(':', ''))
     os.makedirs(film_directory, exist_ok=True)
 
-for letter in string.ascii_uppercase:
-    letter_directory = os.path.join(film_directory, letter)
-    print(film_directory)
-    os.makedirs(letter_directory, exist_ok=True)
+    for letter in string.ascii_uppercase:
+        letter_directory = os.path.join(film_directory, letter)
+        os.makedirs(letter_directory, exist_ok=True)
 
-    # Цикл для кожної нагороди фільму
-    for award in sorted_awards_list:
-        award_name = award["award_name"]
-        for first_letter_award in award_name[0]:
+        # Цикл для кожної нагороди фільму
+        for award in sorted_awards_list:
+            award_name = award["award_name"]
+            first_letter_award = award_name[0]
             if first_letter_award == letter:
+                # Create the award file path within the loop
                 award_file_path = os.path.join(letter_directory, f"{award_name}.txt")
 
-
-        # Створення або дописання інформації в файл
-        with open(award_file_path, "w", encoding='utf-8') as award_file:
-            award_file.write(f"Nominations for {award['award']} in {film_title}:\n")
+                # Створення або дописання інформації в файл
+                with open(award_file_path, "a", encoding='utf-8') as award_file:
+                    # Цикл для кожної номінації нагороди
+                     nomination = award['award']
+                     award_file.write(f"{nomination}\n")
