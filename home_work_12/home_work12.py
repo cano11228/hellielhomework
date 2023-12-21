280ebddd607ae8d97833be0a0df241443b3f1966
cano <yra06052005@gmail.com>
2023-12-21 18:29:05 +0200

    .\home_work_12\

--- home_work_12/home_work12.py
+++ home_work_12/home_work12.py
@@ -20,16 +20,14 @@ for film in films_titles["results"]:
         letter_directory = os.path.join(film_directory, letter)
         os.makedirs(letter_directory, exist_ok=True)
 
-        # Цикл для кожної нагороди фільму
+
         for award in sorted_awards_list:
             award_name = award["award_name"]
             first_letter_award = award_name[0]
             if first_letter_award == letter:
-                # Create the award file path within the loop
                 award_file_path = os.path.join(letter_directory, f"{award_name}.txt")
 
-                # Створення або дописання інформації в файл
+
                 with open(award_file_path, "a", encoding='utf-8') as award_file:
-                    # Цикл для кожної номінації нагороди
                      nomination = award['award']
                      award_file.write(f"{nomination}\n")
